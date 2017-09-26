d7e7de3993943aeac7658291ad4b28483f7d0efd
Nikhil Vengal <nikhilvengal@gmail.com>
2017-09-25 22:54:18 -0400

    Added more keywords

--- CommentSorter/src/main/CommentSorter.java
+++ CommentSorter/src/main/CommentSorter.java
@@ -55,20 +55,21 @@ public class CommentSorter {
         System.out.println("-----------COMMENTS SUCCESSFULLY SORTED TO OUTPUT.CSV-----------");
     }
     
+    //all keywords should be lower case
     public static void initialize() {
-        keywords.put("Clarity of Direction", new String[]{"restructure", "pressure", "change", "goals", "confused", "goal setting", "sales culture", "unrealistic", "expectation"});
-        keywords.put("Management Behaviors", new String[]{"manager", "fear", "favoritism", "respect"});
+        keywords.put("Clarity of Direction", new String[]{"score", "quota", "restructure", "pressure", "change", "goal", "confused", "goal setting", "sales culture", "unrealistic", "expectation"});
+        keywords.put("Management Behaviors", new String[]{"manager", "fear", "favoritism", "respect", "micromanage"});
         keywords.put("Communication", new String[]{"consolidation", "communication", "leadership", "management", "opinion", "input", "disconnect"});
-        keywords.put("Teamwork And Collaboration", new String[]{"teamwork", "collaborate", "team", "partnerships", "cooperation", "department", "5's"});
-        keywords.put("Diversity And Inclusion", new String[]{"diversity", "inclusion", "equal"});
-        keywords.put("Continuous Improvement", new String[]{"exceed"});
-        keywords.put("Wellness And Resiliency", new String[]{"stress", "disconnect", "healthy"});
-        keywords.put("Growth And Development", new String[]{"growth", "development", "career", "grow", "promote", "promoting", "promotion", "training"});
+        keywords.put("Teamwork & Collaboration", new String[]{"colleague", "consistency", "teamwork", "collaborate", "team", "partnerships", "cooperation", "department", "5's"});
+        keywords.put("Diversity & Inclusion", new String[]{"considerate", "diversity", "inclusion", "equal"});
+        keywords.put("Continuous Improvement", new String[]{"exceed", "redundancy"});
+        keywords.put("Wellness & Resiliency", new String[]{"stress", "disconnect", "healthy", "health", "exercise"});
+        keywords.put("Growth & Development", new String[]{"advancement", "growth", "development", "career", "grow", "promote", "promoting", "promotion", "training"});
         keywords.put("Engagement", new String[]{"engage", "engaged", "engagement"});
-        keywords.put("Resources And Support", new String[]{"apparel", "hiring", "staffing", "technology", "security", "stools", "chairs", "support", "turnover"});
-        keywords.put("Pride in Company", new String[]{"customer service", "service", "community", "communities", "brand", "recognize", "recognition"});
-        keywords.put("Reward And Recognition", new String[]{"pay", "benefits", "compensation", "incentive", "insurance", "health", "wellness"});
-        keywords.put("Employee Empowerment", new String[]{"safe", "balance", "family", "work-life balance", "work life balance", "schedule", "scheduling", "hours", "call nights"});
+        keywords.put("Resources & Support", new String[]{"mentor", "duplicate", "licensed", "budget", "deposit", "computer", "saturday", "apparel", "dress", "hire", "hiring", "staff", "technology", "security", "stools", "chairs", "support", "turnover"});
+        keywords.put("Pride in Company", new String[]{"branch", "value", "customer", "service", "dispute", "community", "communities", "brand", "recognize", "recognition"});
+        keywords.put("Reward & Recognition", new String[]{"produce", "celebrating", "valuable", "money", "credit", "pay", "benefits", "compensation", "incentive", "insurance", "health", "wellness"});
+        keywords.put("Employee Empowerment", new String[]{"offers", "coaching", "decisions", "safe", "balance", "family", "work-life balance", "work life balance", "schedule", "scheduling", "hours", "call nights"});
     }
     
     private static CellProcessor[] getProcessors() {
